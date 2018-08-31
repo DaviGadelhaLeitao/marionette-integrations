2deb15bcede61d9a3f00819917f6f07497359a10
davi.leitao <dgleitao.contratos@gmail.com>
2018-08-31 13:45:02 -0300

    renderizando um li na tela inicial com uma variável pertecente a um model

--- webpack2-mocha/app/components/ItemView.js
+++ webpack2-mocha/app/components/ItemView.js
@@ -1,6 +1,12 @@
 import Marionette from 'backbone.marionette';
 import template from '../templates/item.jst';
+import { PessoaModel } from '../models/PessoaModel';
 
-export default Marionette.View.extend({
-  template: template
+const ItemView = Marionette.View.extend({
+  template: template,
+  model: new PessoaModel({
+    nome: "davi"
+  })
 });
+
+export default ItemView;
